c3ddcbf9780a7bddb0966d25143dc2a79ff88179
Dandy <dnyajbr24@gmail.com>
2026-01-19 03:06:51 +0000

    versi 1.1 - ganjil dan genap

---
+++ main.py
@@ -0,0 +1,8 @@
+bil = int(input("Masukkan bilangan:"))
+
+if bil % 2 == 0:
+    print(bil,"Adalah bilangan genap")
+else:
+    print (bil,"Adalah ganjil")
+
+print("kode ini saya buat dengan codespace")
